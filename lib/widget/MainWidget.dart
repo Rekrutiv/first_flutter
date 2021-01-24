df0a7474546d41abb8ab830c2851d717bdf2d363
VOLODYMYR <rekrutivvolodymyr@gmail.com>
2021-01-24 14:36:46 +0200

    create and extract widget

---
+++ lib/widget/MainWidget.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../ColorProvider.dart';
+
+class  MainWidget extends StatelessWidget {
+  const MainWidget({
+    Key key,
+    @required this.currentColor,
+    @required this.counterClick,
+  }) : super(key: key);
+
+  final Color currentColor;
+  final ColorProvider counterClick;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200,
+      height: 200,
+      decoration:
+      BoxDecoration(color: currentColor, shape: BoxShape.circle,
+      ),
+
+      child: Consumer<ColorProvider>(
+        builder: (_, colorProvider, __) => Text(
+            "Total price: ${counterClick.mainText}",
+            style: TextStyle(
+                color: Colors.green[700],
+                fontWeight: FontWeight.w500,
+                fontFamily: 'Open Sans',
+                fontSize: 20)),
+      ),
+    );
+  }
+}
